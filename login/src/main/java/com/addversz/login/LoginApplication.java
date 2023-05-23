e67638587219ec438ebe9e8dea9d1b9b2a5a49b4
sivaprakash balusamy <siva.jfullstackdeveloper@gmail.com>
2023-05-23 17:51:44 +0530

    add service in main method

--- login/src/main/java/com/addversz/login/LoginApplication.java
+++ login/src/main/java/com/addversz/login/LoginApplication.java
@@ -2,10 +2,12 @@ package com.addversz.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@Service
 public class LoginApplication {
 
 	public static void main(String[] args) {
